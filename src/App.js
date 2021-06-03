3783028bf0dce426200b4e23ab4f8979c6099cb9
bartlomiejra <bartlomiej.ra@gmail.com>
2021-06-03 19:03:15 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Card, CardGrid, Container, Header } from "./Elements";
 import "./App.css";
 import Menu from "./Menu";
@@ -6,18 +6,47 @@ import blue from "./blue.png";
 import purp from "./purp.png";
 import black from "./black.png";
 import green from "./green.png";
+import {motion} from 'framer-motion'
+
+//B defoult all transforms are 3d
+// you should only animate transfroms and op
 
 function App() {
+	const [value, setValue] = useState(0)
+	const [isToggled, setToggle] = useState(1)
   return (
-    <div>
+    <motion.div	initial={{opacity: 0}}
+animate={{opacity: 1}}
+transition={{duration: 1}} 
+
+	>
       <Header>
         <Menu />
         <h1>Header</h1>
       </Header>
       <Container>
-        <h2>Super Cool</h2>
-        <CardGrid>
-          <Card style={{ background: "var(--purp)" }}>
+        <motion.h2
+		animate={{
+			opacity: isToggled,
+			x: value * 5 + "px"}}
+		>Super Cool</motion.h2>
+		<button onClick={ ( )=> setToggle((prevValue) => {
+			return prevValue ? 0 : 1;
+		
+		})
+	}
+		>Toggle</button>
+		<input 
+		type="range" 
+		min="-100"
+		 max="100" 
+		 value={value} 
+		 onChange= {e => setValue(e.target.value)} />
+        <CardGrid >
+          <Card 
+		  
+	 style={{ background: "var(--purp)" }}
+	 >
             <h3>Some card</h3>
             <img src={purp} />
           </Card>
@@ -35,7 +64,7 @@ function App() {
           </Card>
         </CardGrid>
       </Container>
-    </div>
+    </motion.div>
   );
 }
 
