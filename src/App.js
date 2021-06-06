82864b4ae7d0cb385cf625f39cdb95dd8f2ac113
bartlomiejra <bartlomiej.ra@gmail.com>
2021-06-07 01:26:30 +0200

    aax

--- src/App.js
+++ src/App.js
@@ -1,71 +1,123 @@
-import React, {useState} from "react";
-import { Card, CardGrid, Container, Header } from "./Elements";
-import "./App.css";
-import Menu from "./Menu";
-import blue from "./blue.png";
-import purp from "./purp.png";
-import black from "./black.png";
-import green from "./green.png";
-import {motion} from 'framer-motion'
-
-//B defoult all transforms are 3d
-// you should only animate transfroms and op
+import React, { useState } from 'react'
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  AnimatePresence,
+} from 'framer-motion'
+import { Card, CardGrid, Container, Header } from './Elements'
+import './App.css'
+import Modal from './Modal'
+import Accordion from './Accordion'
+import Nav from './Nav'
+import Squeres from './Squeres'
+import Menu from './Menu'
+import blue from './blue.png'
+import purp from './purp.png'
+import black from './black.png'
+import green from './green.png'
 
 function App() {
-	const [value, setValue] = useState(0)
-	const [isToggled, setToggle] = useState(1)
-  return (
-    <motion.div	initial={{opacity: 0}}
-animate={{opacity: 1}}
-transition={{duration: 1}} 
+  const [value, setValue] = useState(0)
+  const [isNavOpen, setIsNavOpen] = useState(false)
+  const [isCardActive, setIsCardActive] = useState(true)
+  const [isToggled, setToggle] = useState(false)
+  const x = useMotionValue(0)
+  const opacity = useTransform(x, [-200, 0, 200], [0, 1, 0])
 
-	>
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
       <Header>
-        <Menu />
+        <Menu onClick={() => setIsNavOpen(true)} />
+        <Nav isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
         <h1>Header</h1>
       </Header>
       <Container>
-        <motion.h2
-		animate={{
-			opacity: isToggled,
-			x: value * 5 + "px"}}
-		>Super Cool</motion.h2>
-		<button onClick={ ( )=> setToggle((prevValue) => {
-			return prevValue ? 0 : 1;
-		
-		})
-	}
-		>Toggle</button>
-		<input 
-		type="range" 
-		min="-100"
-		 max="100" 
-		 value={value} 
-		 onChange= {e => setValue(e.target.value)} />
-        <CardGrid >
-          <Card 
-		  
-	 style={{ background: "var(--purp)" }}
-	 >
+        <Squeres />
+        {isToggled && <h2>Super Coool</h2>}
+        <button onClick={() => setToggle(true)}>Toggle</button>
+        <input
+          type="range"
+          min="-100"
+          max="100"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Modal isToggle={isToggled} setToggle={setToggle}>
+          <Card style={{ background: 'var(--purp)' }}>
             <h3>Some card</h3>
-            <img src={purp} />
+            <img src={purp} alt="photso" />
           </Card>
-          <Card style={{ background: "var(--blue)" }}>
+        </Modal>
+        <Accordion />
+        <CardGrid>
+          <Card
+            //    whileHover ={{ scale: [1.02, 0.8 ,1.04]}}
+            //    whileTap={{ background: "var(--red)"}}
+            //    onHoverEnd={( ) => console.log("hover end")}
+
+            drag
+            dragConstraints={{
+              top: -100,
+              left: -100,
+              bottom: 100,
+              right: 100,
+            }}
+            style={{ background: 'var(--purp)' }}
+          >
             <h3>Some card</h3>
-            <img src={blue} />
+            <img src={purp} alt="photso" />
           </Card>
-          <Card style={{ background: "var(--black)" }}>
+          <AnimatePresence>
+            {isCardActive && (
+              <motion.div
+                exit={{ height: 0, overflow: 'hidden', opacity: 0 }}
+                transition={{
+                  opacity: {
+                    duration: 0,
+                  },
+                }}
+              >
+                <Card
+                  onDragEnd={(event, info) => {
+                    console.log(info.point.x)
+                    if (Math.abs(info.point.x) > 200) {
+                      setIsCardActive(false)
+                    }
+                  }}
+                  drag="x"
+                  dragConstraints={{
+                    left: 0,
+                    right: 0,
+                  }}
+                  style={{
+                    x,
+                    opacity,
+                    background: 'var(--blue)',
+                  }}
+                >
+                  <h3>Some card</h3>
+                  <img src={blue} alt="photosdw" />
+                </Card>
+              </motion.div>
+            )}
+          </AnimatePresence>
+          <Card style={{ background: 'var(--black)' }}>
             <h3>Some card</h3>
-            <img src={black} />
+            <img src={black} alt="photrro" />
           </Card>
-          <Card style={{ background: "var(--green)" }}>
+          <Card style={{ background: 'var(--green)' }}>
             <h3>Some card</h3>
-            <img src={green} />
+            <img src={green} alt="pho45to" />
           </Card>
         </CardGrid>
       </Container>
     </motion.div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Nav.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import {motion} from 'framer-motion'
+import styled from 'styled-components'
+
+const variants = {
+	open: { x: 0},
+closed: { x: "-100%", transition:{
+	delay: 0.2}
+}
+}
+const livariants = {
+	open: {y: 0, opacity: 1,
+	},
+closed: { y: -20, opacity: 0,
+},
+
+
+}
+
+const ulvariants = {
+	open: {
+		scale: 1.05,
+		transition:{
+			staggerChildren: 0.3,
+			deleyChildren: 0.2,
+			staggerDirection: -1, // 1 forwards, -1 backwards
+			when: "afterChildren" //"afterChildren" "beforeChildren"
+			
+	}
+},
+closed: {
+scale: 1
+}
+};
+
+
+
+const links = [
+	"one","two"
+, "three", "four"]
+export const Nav = ({isNavOpen, setIsNavOpen}) => {
+	return (
+		<MenuNav
+		 variants={variants} 
+		 initial="closed" 
+		 animate={isNavOpen ? 'open' : 'closed'}
+		 transition={{damping: 10}}
+		 >
+			<button onClick={() => setIsNavOpen(false)}>Close</button>
+			<motion.ul variants={ulvariants}>
+				{links.map(link => (
+					<motion.li variants={livariants} key={link}>
+						
+						<a href="#">{link}</a></motion.li>
+				))}
+				
+				
+			</motion.ul>
+		</MenuNav>
+	)
+}
+
+
+const MenuNav = styled(motion.nav)`
+position: fixed;
+top: 0;
+left: 0;
+width: 100vw;
+height: 100vh;
+background: var(--black) ;
+padding: 40px;
+
+
+ul{
+	list-style: none;
+padding: 0;
+margin: 0;
+}
+li{
+	padding: 0;
+	margin: 0 0 1rem; 
+font-size: 2rem;
+	a{
+		text-decoration: none;
+		border-bottom: 2px transparent solid;
+		transition: 0.3s ease border;
+		color: white;
+		&:hover{
+			
+			border-bottom: 2px var(--blue) solid;
+		}
+
+}
+}
+
+
+`;
+export default Nav;
